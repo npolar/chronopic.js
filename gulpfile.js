2931b3092e88f6abb25176bf839981ea502448b3
Remi Aleksander Solås <remi@npolar.no>
2015-07-29 14:47:50 +0200

    Changed distribution file extensions to .min.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,28 @@
 var gulp = require('gulp');
 
+var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
 var jshint = require('gulp-jshint');
 var csso = require('gulp-csso');
 
 gulp.task('minify-js', function() {
-	return gulp.src("chronopic.js")
+	return gulp.src('src/chronopic.js')
 	.pipe(uglify())
+	.pipe(rename(function(path) { path.basename += ".min" }))
 	.pipe(gulp.dest('./dist'));
 });
 
 gulp.task('minify-i18n', function() {
 	return gulp.src('src/i18n/*.js')
 	.pipe(uglify())
+	.pipe(rename(function(path) { path.basename += ".min" }))
 	.pipe(gulp.dest('./dist/i18n'));
 });
 
 gulp.task('minify-css', function() {
 	return gulp.src('src/css/*.css')
 	.pipe(csso())
+	.pipe(rename(function(path) { path.basename += ".min" }))
 	.pipe(gulp.dest('./dist/css'));
 });
 
