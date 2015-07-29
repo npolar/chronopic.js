6f0acd15d8e2d894eddf393b663856f554a386a5
Remi Aleksander Solås <remi@npolar.no>
2015-07-29 14:21:52 +0200

    Added ja_JP, nb_NO and nn_NO locales

---
+++ i18n/nb_NO.js
@@ -0,0 +1,21 @@
+(function() {
+	Chronopic.i18n.nb_NO = {
+		dayOfWeek:		[ "Søndag", "Mandag", "Tirsdag", "Onsdag", "Torsdag", "Fredag", "Lørdag" ],
+		dayOfWeekShort:	[ "Søn", "Man", "Tir", "Ons", "Tor", "Fre", "Lør" ],
+		disabled:		"Deaktivert",
+		formatDay:		"{DDDD} {D}. {MMMM} {YYYY}",
+		formatMonth:	"{MMMM} {YYYY}",
+		formatYear:		"{YYYY}",
+		month:			"Måned",
+		monthName:		[ "Januar", "Februar", "Mars", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Desember" ],
+		monthNameShort:	[ "Jan", "Feb", "Mar", "Apr", "Mai", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Des" ],
+		nextMonth:		"Neste måned",
+		nextYear:		"Neste år",
+		prevMonth:		"Forrige måned",
+		prevYear:		"Forrige år",
+		selectMonth:	"Velg måned",
+		selectYear:		"Velg år",
+		week:			"Uke",
+		year:			"År"
+	};
+})();
