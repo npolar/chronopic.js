dd8cacaa3c7c3fb8c6b9a1620f53af6f7753efea
Remi Aleksander Solås <remi@npolar.no>
2015-07-30 17:01:47 +0200

    Added en_US locale

---
+++ src/i18n/en_US.js
@@ -0,0 +1,19 @@
+(function() {
+	Chronopic.i18n.en_US = {
+		dayOfWeek:		[ "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" ],
+		dayOfWeekShort:	[ "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" ],
+		disabled:		"Disabled",
+		formatDay:		"{DDDD} {MMMM} {D}, {YYYY}",
+		formatMonth:	"{MMMM} {YYYY}",
+		month:			"Month",
+		monthName:		[ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ],
+		monthNameShort:	[ "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" ],
+		nextMonth:		"Next month",
+		nextYear:		"Next year",
+		prevMonth:		"Previous month",
+		prevYear:		"Previous year",
+		selectMonth:	"Select month",
+		week:			"Week",
+		year:			"Year"
+	};
+})();
