ef7d805b533daae719d389b11e2e7d77ed66ff6b
RDuX <ruben.dens@gmail.com>
2015-07-30 16:59:23 +0200

    Added support for the Dutch language

---
+++ src/i18n/nl_BE.js
@@ -0,0 +1,19 @@
+(function() {
+	Chronopic.i18n.nl_BE = {
+		dayOfWeek:		[ "zondag", "maandag", "dinsdag", "woensdag", "donderdag", "vrijdag", "zaterdag" ],
+		dayOfWeekShort:	[ "zo", "ma", "di", "woe", "do", "vrij", "zat" ],
+		disabled:		"gedeactiveerd",
+		formatDay:		" {D} {MMMM} {YYYY}",
+		formatMonth:	"{MMMM} {YYYY}",
+		month:			"maand",
+		monthName:		[ "januari", "februari", "maart", "april", "mei", "juni", "juli", "augustus", "september", "oktober", "november", "december" ],
+		monthNameShort:	[ "jan", "feb", "mrt", "apr", "mei", "jun", "jul", "aug", "sep", "okt", "nov", "dec" ],
+		nextMonth:		"volgende maand",
+		nextYear:		"volgend jaar",
+		prevMonth:		"vorige maand",
+		prevYear:		"vorig jaar",
+		selectMonth:	"selecteer maand",
+		week:			"week",
+		year:			"jaar"
+	};
+})();
