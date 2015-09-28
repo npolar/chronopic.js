89b95b864a10de4c260c2e83d00fe44b34b2b32a
Remi Aleksander Solås <remi@npolar.no>
2015-09-28 10:05:52 +0200

    Updated translations to comply with the new specifications

--- src/i18n/nn_NO.js
+++ src/i18n/nn_NO.js
@@ -1,18 +1,22 @@
 (function() {
 	Chronopic.i18n.nn_NO = {
+		anteMeridiem:	"",
 		dayOfWeek:		[ "sundag", "måndag", "tysdag", "onsdag", "torsdag", "fredag", "laurdag" ],
 		dayOfWeekShort:	[ "su", "må", "ty", "on", "to", "fr", "lau" ],
 		disabled:		"deaktivert",
-		formatDay:		"{DDDD} {D}. {MMMM} {YYYY}",
-		formatMonth:	"{MMMM} {YYYY}",
+		formatDate:		"{D}. {MMMM} {YYYY}",
+		formatDateTime:	"{D}. {MMMM} {YYYY}, {HH}:{mm}",
 		month:			"Månad",
 		monthName:		[ "januar", "februar", "mars", "april", "mai", "juni", "juli", "august", "september", "oktober", "november", "desember" ],
 		monthNameShort:	[ "jan", "feb", "mars", "apr", "mai", "juni", "juli", "aug", "sep", "okt", "nov", "des" ],
 		nextMonth:		"Neste månad",
 		nextYear:		"Neste år",
+		postMeridiem:	"",
 		prevMonth:		"Førre månad",
 		prevYear:		"Førre år",
 		selectMonth:	"Velj månad",
+		titleDay:		"{DDDD} {D}. {MMMM} {YYYY}",
+		titleMonth:		"{MMMM} {YYYY}",
 		week:			"Veke",
 		year:			"År"
 	};
